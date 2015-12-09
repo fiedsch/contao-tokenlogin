49be2378a1a16b2e2ef43b70d12119a24a904959
fiedsch <fiedsch@ja-eh.at>
2015-12-09 10:05:14 +0100

    added german language files

---
+++ stammtisch/languages/de/modules.php
@@ -0,0 +1,19 @@
+<?php
+
+$GLOBALS['TL_LANG']['FMD']['stammtisch'] = 'Contao Stammtisch München';
+
+$GLOBALS['TL_LANG']['FMD']['tokenlogin'] = array(
+    'Token Login',
+    'Erweitertes Login-Modul das einen Login mit einem Token ermöglicht (anstelle username + password des "normalen" Login-Moduls)'
+);
+
+$GLOBALS['TL_LANG']['FMD']['toklog_token'] = 'Token';
+
+$GLOBALS['TL_LANG']['FMD']['toklog_slabel'] = 'Los!';
+
+$GLOBALS['TL_LANG']['FMD']['logtok_emptyField'] = 'Bitte geben Sie ein Token ein';
+
+$GLOBALS['TL_LANG']['FMD']['logtok_invalidLogin'] = 'Bitte geben Sie ein gültiges Token ein';
+
+$GLOBALS['TL_LANG']['FMD']['logtok_loggedInAs'] = 'Login mit Token %s war erfolgreich';
+
